239d65606ed96ed74097395191182e2075c0c0bc
Evan Borgstrom <evan@fatbox.ca>
2011-08-10 21:12:27 -0400

    Exact versions needed
    Newer versions of oauth2 and httplib cause problems, will need to
    investigate further...

--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ setup(
         version="1.0",
         packages = ['xero',],
         install_requires=[
-            'httplib2>=0.6.0',
-            'oauth2>=1.2.0',
-            'SocksiPy-branch>=1.02',
+            'httplib2==0.6.0',
+            'oauth2==1.2.0',
+            'SocksiPy-branch==1.02',
             ],
         dependency_links=[
             'http://socksipy-branch.googlecode.com/issues/attachment?aid=30003000&name=SocksiPy-branch-1.02.tar.gz&token=61d76a1fa803a56bcfd1a93e34365158#egg=SocksiPy-branch-1.02',
